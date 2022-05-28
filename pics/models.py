6c0dd086bed7ce6c22047a8f7a774be76a60c939
haimy254 <haimanaare@gmail.com>
2022-05-28 13:24:34 +0300

    initial commit

---
+++ pics/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from distutils.command.upload import upload
+
+# Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=80)
+    
+    def __str__(self):
+        return self.location
+    
+    def save_location(self):
+        self.save
+        
+class Category(models.Model):
+    category = models.CharField(max_length=80)
+    
+    def __str__(self):
+        return self.category
+    
+    def save_category(self):
+        self.save
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='image/',default='')
+    image_names = models.CharField(max_length=100)
+    image_description= models.CharField(max_length=500)
+    location = models.ForeignKey(Location,on_delete=models)
+    category = models.ForeignKey(Category, on_delete=models)
+    
+
+    
