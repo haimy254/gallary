6307cdd688f1d98227d9c186bf00a9bbec577838
haimy254 <haimanaare@gmail.com>
2022-05-29 04:13:32 +0300

    initial views of image in the views.py

--- pics/views.py
+++ pics/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import *
+
+# Create your views here.
+def image_view(request):
+
+	if request.method == 'POST':
+		form = ImageForm(request.POST, request.FILES)
+
+		if form.is_valid():
+			form.save()
+			return redirect('success')
+	else:
+		form = ImageForm()
+	return render(request, 'photo/imageform.html', {'form' : form})
+
+
+def success(request):
+	return HttpResponse('successfully uploaded')
+
 
 # Create your views here.
