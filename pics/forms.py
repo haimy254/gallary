9b956899ab44223f32bb492015685f3582fc67aa
haimy254 <haimanaare@gmail.com>
2022-05-29 03:28:08 +0300

    added location dropdown list in form

--- pics/forms.py
+++ pics/forms.py
@@ -11,12 +11,21 @@ CATEGORY_CHOICE=[
     ('car','Cars'),
     ('nature','Nature'),
 ]
+
+#creating a list for locations
+LOCATION_CHOICE=[
+     ('nairobi','Nairobi'),
+    ('nakuru','Nakuru'),
+    ('mombasa','Mombasa'),
+     ('kisumu','Kisumu'),
+]
 #creating forms for models 
 class ImageForm(forms.ModelsForm):
     class meta:
         image = forms.ImageField(lable='upload image')
         image_names= forms.CharField(lable = 'image name')
         image_description=forms.CharField(lable='description of the image')
+        location = forms.CharField(lable='select image location',widget=forms.Select(choices=LOCATION_CHOICE))
         category = forms.CharField(lable='select image category',widget=forms.Select(choices=CATEGORY_CHOICE))
         
         
